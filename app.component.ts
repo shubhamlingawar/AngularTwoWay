83aae8d6f1f431cfb30f5e97fba6e5693386b239
shubhamlingawar <32120350+shubhamlingawar@users.noreply.github.com>
2020-03-25 14:09:03 +0530

    commit

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name:null;
+  password:null;
+  email:null;
+city:null;
+country:null;
+}
